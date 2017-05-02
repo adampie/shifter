644b248de8a9035be3d7955c9aaee86f7da02a56
Adam Pietrzycki <adam@pietrzycki.com>
2017-05-03 00:04:47 +0100

    Fixing URL problems

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 cd /var/www/html && sudo -H -u adampie mv .env.example .env
 cd /var/www/html && sudo -H -u adampie php artisan key:generate
 cd /var/www/html && sudo -H -u adampie php artisan migrate
-cd /var/www/html && screen -S shifter -d -m php artisan serve
+cd /var/www/html && screen -S shifter -d -m php -S 0.0.0.0:8080 -t public/
 cd /home/adampie && screen -S caddy -d -m caddy -conf=Caddyfile
