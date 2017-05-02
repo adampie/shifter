fa8f73a81f55f8f81f9ab2082f49ba0552059f01
Adam Pietrzycki <adam@pietrzycki.com>
2017-05-03 00:02:31 +0100

    Head reset

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+cd /var/www/html && sudo -H -u adampie mv .env.example .env
 cd /var/www/html && sudo -H -u adampie php artisan key:generate
 cd /var/www/html && sudo -H -u adampie php artisan migrate
 cd /var/www/html && screen -S shifter -d -m php artisan serve
