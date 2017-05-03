66f7363c321c680675fc6aba37cdd65285e7f087
adampie <adam@pietrzycki.com>
2017-05-03 10:02:09 +0100

    Temp

--- shifter/resources/assets/js/components/Employees.vue
+++ shifter/resources/assets/js/components/Employees.vue
@@ -4,11 +4,19 @@
         <a href="#" class="btn pull-right btn-xs btn-primary"><span class="glyphicon glyphicon-align-justify"></span></a>
     </div>
     <div class="panel-body">
+      {{ list }}
     </div>
   </div>
 </template>
 
 <script>
+export default {
+        data() {
+          return {
+            list: 'Hello Pagekit!'
+          }
+        }
+      }
 </script>
 
 <style lang="css">
